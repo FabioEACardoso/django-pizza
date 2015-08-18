d62e9fa1396a34c0b0eb115592cc729798dd813a
Fabio Eduardo Araujo Cardoso <fabio.e.a.cardoso@gmail.com>
2015-08-18 10:48:39 -0300

    Mario e Luigi

---
+++ pyexemples/loops.py
@@ -0,0 +1,15 @@
+pedidos = [
+    {
+        'nome': 'Mario',
+        'sabor': 'Portuguesa'
+    },
+    {
+        'nome': 'Luigi',
+        'sabor': 'Presunto'
+    }
+]
+
+for pedido in pedidos:
+    print(pedido['nome'], pedido['sabor']) 
+
+
