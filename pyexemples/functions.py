c74f5ac18fa693986c1b97e226721907910b7757
Fabio Eduardo Araujo Cardoso <fabio.e.a.cardoso@gmail.com>
2015-08-18 11:33:47 -0300

    funcao de mostrar ou nao observacao

--- pyexemples/functions.py
+++ pyexemples/functions.py
@@ -1,14 +1,21 @@
 pedidos = []
 
 
-def criar_pedido(nome, sabor, observacao='sem observacoes'):
+def criar_pedido(nome, sabor, observacao=None):
     pedido = {}
     pedido['nome'] = nome
     pedido['sabor'] = sabor
     pedido['observacao'] = observacao
     return pedido
 
-print(pedidos)
+
 pedidos.append(criar_pedido('Mario', 'pepperoni'))
-pedidos.append(criar_pedido('Luigi', 'mozzarela'))
-print(pedidos)
+pedidos.append(criar_pedido('Luigi', 'mozzarela', 'dobro de queijo'))
+
+for pedido in pedidos:
+    template = 'Nome: {nome}\nSabor: {sabor}'
+    print(template.format(**pedido))
+    if pedido['observacao']:
+        print('Observacao = {}'.format(pedido['observacao']))
+
+    print('-'*30)
