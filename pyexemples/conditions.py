41235b63fbc995c6335ec35741205dd475b9bfb2
Fabio Eduardo Araujo Cardoso <fabio.e.a.cardoso@gmail.com>
2015-08-18 09:39:14 -0300

    conditions.py

--- pyexemples/conditions.py
+++ pyexemples/conditions.py
@@ -1,2 +1,15 @@
 
 print('Ola Mundo')
+
+salse = 'tomato'
+
+order_description = """
+The client asked for a pizza with less cheese.
+Istead of cheese he would like to have more ham. 
+"""
+
+if salse.startswith('toma'):
+    print("it's probably tomato...")
+else:
+    print("not tomato")
+
